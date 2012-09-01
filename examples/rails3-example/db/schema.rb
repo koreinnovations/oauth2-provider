5195a54e711a3f5d3cf7fe2514310e0a67d10277
Noah Lively <noah@koremedia.net>
2012-09-01 12:35:12 -0700

    updated gem and schema

--- examples/rails3-example/db/schema.rb
+++ examples/rails3-example/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110614224516) do
+ActiveRecord::Schema.define(:version => 20110508151948) do
 
   create_table "accounts", :force => true do |t|
     t.string "login",    :null => false
